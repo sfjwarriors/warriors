05c4e9891be66f6d87732d10c372b6d18a928b54
santiago lopez <santiago.lopez-o@mail.escuelaing.edu.co>
2020-09-05 21:10:40 -0500

    inicio

---
+++ src/main/java/edu/escuelaing/arsw/App.java
@@ -0,0 +1,20 @@
+package edu.escuelaing.arsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {"edu.escuelaing.arsw"})
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+        System.out.println( "Hello World!" );
+    }
+}
